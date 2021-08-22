553ce6b8cd1d7b0a9eaaa5a7244978d1e34bb1d8
takuyakimura <takuya.1117@gmail.com>
2021-08-22 10:13:06 +0900

    DBへの書き込みをプリペアードステートメントに変更

--- redirect.php
+++ redirect.php
@@ -4,9 +4,19 @@
      $content = $_POST['content'];
    
      $db = new PDO('mysql:host=localhost;dbname=php_learning', 'root', '_xgH4gF!riAf');
+
+     $sql = "INSERT INTO board02 (id,author,content,time)
+     VALUES (NULL,:author,:content,NOW())";
    
-     $db->query("INSERT INTO board02 (id,author,content,time)
-           VALUES (NULL,'$author','$content',NOW())");
+     $stmt = $db -> prepare($sql);
+
+     $stmt -> bindValue(':author', $author);
+     $stmt -> bindValue(':content', $content);
+
+     $stmt -> execute();
+
+//     $db->query("INSERT INTO board02 (id,author,content,time)
+//           VALUES (NULL,'$author','$content',NOW())");
 
      header('Location: /index.php', true, 302);
      exit();
